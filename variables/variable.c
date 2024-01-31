28682b68a1cd530bb2704cb8449e96374b3df05c
Poorani <tpoorani2002@gmail.com>
2024-01-31 12:03:23 +0530

    updated

---
+++ variables/variable.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int a=010;
+    char b='a';
+    float c =12.23f;
+    long int d=234533L;
+    int e = 41*19;
+    float f =90.8987789;
+
+    printf("integer -- int --: %d\ncharacter--char--: %c\nfloat--float : %0.2f\nlong int : %d \n%d\n%0.3f",a,b,c,d,e,f);
+
+        return 0;
+    }
+    
