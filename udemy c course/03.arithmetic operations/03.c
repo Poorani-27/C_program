4d43c59b4eb49229d06692973da522a33d978262
Poorani <tpoorani2002@gmail.com>
2024-03-31 23:08:02 +0530

    2

---
+++ udemy c course/03.arithmetic operations/03.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    int a,b,sum,diff,mul,quo,rem;
+    printf("Enter two numbers : ");
+    scanf("%d%d",&a,&b);
+    printf("a= %d, b= %d",a,b);
+
+    return 0;
+}
